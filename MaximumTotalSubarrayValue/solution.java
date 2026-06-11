c12d2db296846d5dd025614562e4bbee3abce390
akhilar97-raw <akhilar97-raw@users.noreply.github.com>
2026-06-11 07:31:28 +0530

    added 0049 leetCode solution

---
+++ MaximumTotalSubarrayValue/solution.java
@@ -0,0 +1,14 @@
+package MaximumTotalSubarrayValue;
+class Solution {
+    public long maxTotalValue(int[] nums, int k) {
+        int mx = nums[0];
+        int mn = nums[0];
+
+        for (int num : nums) {
+            mx = Math.max(mx, num);
+            mn = Math.min(mn, num);
+        }
+
+        return 1L * k * (mx - mn);
+    }
+}
