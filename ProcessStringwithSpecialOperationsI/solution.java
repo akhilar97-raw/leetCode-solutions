2f556b541c4e569bf27235646c78f8afc1f5362c
akhilar97-raw <akhilar97-raw@users.noreply.github.com>
2026-06-17 18:50:11 +0530

    added 0099 question

---
+++ ProcessStringwithSpecialOperationsI/solution.java
@@ -0,0 +1,20 @@
+package ProcessStringwithSpecialOperationsI;
+
+public class solution {
+    public String processStr(String s) {
+        StringBuilder result = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (Character.isLetter(c)) {
+                result.append(c);
+            } else if (c == '*') {
+                result.setLength(Math.max(0, result.length() - 1));
+            } else if (c == '#') {
+                result.append(result);
+            } else if (c == '%') {
+                result.reverse();
+            }
+        }
+        return result.toString();
+    }
+}
+
